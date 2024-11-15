1562163cf646b3ce4339b09c34307c71fb0c4693
simoniz0r <bnu.fantasma@gmail.com>
2024-11-15 17:01:42 -0600

    Simplify script using nushell

--- commands/info/supported-json-to-markdown.sh
+++ commands/info/supported-json-to-markdown.sh
@@ -1,24 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env nu
 # Author: Syretia
 # License: MIT
-# Dependencies: jq
+# Dependencies: nushell
 # run this script from the base directory of the git repo and redirect the output to './commands/info/supported.md'
 
-# create array of keys from first entry to use as headers of table
-mapfile -t headers < <(jq -r '.[0] | keys_unsorted[]' ./commands/info/supported.json)
-# output headers in table format
-echo -n '|'
-for header in "${headers[@]}"; do
-	header="$(echo "$header" | tr '_' ' ' | sed -e "s/\b\(.\)/\u\1/g")"
-	echo -n " $header |"
-done
-# output a separator for each header
-echo
-echo -n '|'
-for header in "${headers[@]}"; do
-	echo -n ' --- |'
-done
-echo
-# output each entry in table format
-jq '.[]' ./commands/info/supported.json | \
-jq -r '[ to_entries | .[].value ] | "| " + join(" | ") + " |"'
+# sort by name, rename columns, output as markdown table
+open ./commands/info/supported.json | sort-by -ni name | rename Name Supported "Experience Level" Recommended Notes | to md
